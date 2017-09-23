535cbca67575e050a0972f9e7e0cb6b493318442
rishabh4782 <rishabh4782@gmail.com>
2017-09-24 00:45:18 +0530

    ui changes

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 var express = require('express');
-
+var path = require('path');
+var bodyParser = require('body-parser')
 var app = express();
 
 //configure app
-
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 // use middleware
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname + 'bower_components')));
+
 
 //define routes
 
 app.get('/', function(req, res){
-  res.send('Good Morning Paras and Gaurav!!')
+  res.render('index');
+});
+
+
+app.post('/proceed', function(req, res){
+  console.log('Proceed clicked');
+  var guestUser = req.body.guestUser;
+  res.redirect('/welcome')
+});
+
+app.get('/welcome', function(req, res){
+  res.render('welcome');
 });
 
 var port = process.env.PORT || 3000;
