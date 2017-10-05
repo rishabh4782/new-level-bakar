6e23b9bf0c10a65db8b57abe60a4300137321651
rishabh4782 <rishabh4782@gmail.com>
2017-10-06 03:21:43 +0530

    adding JS cart

---
+++ models/product.js
@@ -0,0 +1,28 @@
+var products = [{
+    imagePath: 'https://www.cashoverflow.in/wp-content/uploads/2017/05/Standard-Chartered-Platinum-Rewards-Card.png?x21615',
+    title: 'Standard Chartered Platinum Rewards Card',
+    description: 'https://apply.standardchartered.co.in/credit-card?selectedCardId=6&lp=no&source=email&se=ICL&cp=ICL_CC_IP_OCT&ag=CC_mailers_longform_prctext_16568',
+    price: 10
+  },{
+    imagePath: 'https://www.cashoverflow.in/wp-content/uploads/2015/04/icici-plat-master.png',
+    title: ' ICICI Instant Platinum Credit Card',
+    description: 'https://www.m-icicibank.com/credit-card-desktop/index.html?se=pr-cc-sf-p2-c11-s11--cta1-380638&cp=icicibank-pr-cc-p2&utm_campaign=icicibank-pr-cc-p2&utm_content=pr-cc-sf-p2-c11-s11-cta1-p2&utm_medium=email&utm_source=email-p2&admitad_uid=295d1ea616775941d2dc247a5e406734',
+    price: 20
+  },{
+    imagePath: 'https://www.cashoverflow.in/wp-content/uploads/2016/05/ALT-Standard-Chartered-manhattan-Credit-card.png',
+    title: 'Standard Chartered Manhattan Credit Card',
+    description: 'https://www.cashoverflow.in/recommends/scbmanhattan/',
+    price: 30
+  },{
+    imagePath: 'https://www.cashoverflow.in/wp-content/uploads/2016/05/citi-cash-back-credit-card.png?x21615',
+    title: 'Citibank Cashback Credit Card',
+    description: 'https://www.online.citibank.co.in/portal/newgen/cards/May17/apply-now.htm?ecid=EM107DINCCAENCB&cardName=cashback&Campaign=External&site=ADMITAD&creative=MAILER&section=EXTMLROCT17ADMITADCBRETURNGIFTCPL_380638&agencyCode=IAPL&campaignCode=FAS&productCode=CARDS&admitad_uid=6b9092f44a841291ad447746a3291c80',
+    price: 40
+  },{
+    imagePath: 'https://www.cashoverflow.in/wp-content/uploads/2016/05/ALT-HDFC-regalia-credit-card.png',
+    title: "HDFC Regalia Credit Card",
+    description: "https://www.hdfcbank.com",
+    price: 50
+  }];
+
+  module.exports = products;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var Cart = require('../models/cart');
+
+var Product = require('../models/product');
+//var Order = require('../models/order');
+
 /* GET home page. */
 //define routes
 
@@ -28,12 +33,76 @@ router.get('/contact', function(req, res){
 });
 
 
-router.post('/cardmall', function(req, res){
-  console.log('cardmall clicked');
-  var guestUser = req.body.guestUser;
-  //console.log('user : ' + guestUser);
-  res.render('cardmall', {userName: guestUser});
+router.post('/cardmall', function(req, res, next) {
+  console.log('cardmall clicked....');
+  res.render('cardmall', {
+      products: Product
+    });
+  });
+
+
+router.get('/add-to-cart/:id', function(req, res, next) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {});
+
+  Product.findById(productId, function(err, product) {
+    if (err) {
+      return res.redirect('/cardmall');
+    }
+    cart.add(product, product.id);
+    req.session.cart = cart;
+    res.redirect('/cardmall');
+  });
+});
+
+router.get('/reduce/:id', function(req, res, next) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {});
+
+  cart.reduceByOne(productId);
+  req.session.cart = cart;
+  res.redirect('/shopping-cart');
+});
+
+router.get('/remove/:id', function(req, res, next) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {});
+
+  cart.removeItem(productId);
+  req.session.cart = cart;
+  res.redirect('/shopping-cart');
+});
+
+router.get('/shopping-cart', function(req, res, next) {
+  if (!req.session.cart) {
+    return res.render('shop/shopping-cart', {
+      products: null
+    });
+  }
+  var cart = new Cart(req.session.cart);
+  res.render('shop/shopping-cart', {
+    products: cart.generateArray(),
+    totalPrice: cart.totalPrice
+  });
+});
+
+router.get('/checkout', isLoggedIn, function(req, res, next) {
+  if (!req.session.cart) {
+    return res.redirect('/shopping-cart');
+  }
+  var cart = new Cart(req.session.cart);
+
+  res.render('shop/checkout', {
+    total: cart.totalPrice,
+    noError: !errMsg
+  });
 });
 
 
 module.exports = router;
+
+function isLoggedIn(req, res, next) {
+  console.log('is authenticated');
+    return next();
+
+}
